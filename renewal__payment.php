968baca33b0ffa412f758599b611b9825842d300
ashwininboyite <32223555+ashwininboyite@users.noreply.github.com>
2018-12-02 19:30:15 +0530

    Update renewal__payment.php

--- renewal__payment.php
+++ renewal__payment.php
@@ -3,9 +3,9 @@
     $con = mysqli_connect("localhost","root","") or die ("Unable to connect");
     mysqli_select_db($con,"library_proj");
     $found=0;
-    #$id=$_POST['id'];
-    $result='';
-    $id='Cs_23';
+    $id=$_POST['id'];
+    $result = array();
+    #$id='Cs_11';
    # $student_name=$_SESSION['stu_username'];
     $student_name='Ashwini';
     #to access the student card number;
@@ -31,6 +31,7 @@
         #echo $amount;
         if($count < $amount)
         {
+            
             $amount = $amount - $count;
             $next_date = date ('Y-m-d', strtotime ($today_str ."+6 days"));
             
@@ -40,13 +41,18 @@
             $query5="UPDATE issue set  to_date = '$next_date' WHERE card_no='$student_id' and Book_id='$id'";
             $query_run_5 = mysqli_query($con,$query5);
             
-            $query6="INSERT INTO payment_details  VALUES ('$id','$student_id',$count,$today_str)";
+            $query6="INSERT INTO payment_details  VALUES ('$id','$student_id','$count','$today_str')";
             $query_run_6 = mysqli_query($con,$query6);
+            
+            $query7="UPDATE borrower set  amount_details = '$amount' WHERE card_no='$student_id'";
+            $query_run_7 = mysqli_query($con,$query7);
+            
+            $result[]='Passed';
             
         }
         else{
-            $result='failed';
+            $result[]='Failed';
         }
     }
-   echo $result;
+        echo json_encode( $result );     
     ?>
